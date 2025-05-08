0cb36441a113a8a3ca396c26471aa1a5b37712e0
yusenshen <yusenshen0831@gmail.com>
2025-05-08 20:02:21 +0800

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://d025-116-15-88-34.ngrok-free.app/api',
+  baseURL: 'https://nation-developed-collect-dimensions.trycloudflare.com/api',
   timeout: 5000,
 })
 

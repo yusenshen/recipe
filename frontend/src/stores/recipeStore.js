86672b7c720da534773b3f094085bcaf18000ee4
yusenshen <yusenshen0831@gmail.com>
2025-05-08 20:04:58 +0800

    Update recipeStore.js

--- frontend/src/stores/recipeStore.js
+++ frontend/src/stores/recipeStore.js
@@ -17,11 +17,7 @@ export const useRecipeStore = defineStore('recipe', {
     actions: {
         async fetchRecipes() {
             try {
-                console.log("fetchRecipes called")
                 const res = await api.get('/recipes')
-                console.log("res =", res)
-                console.log("res.data =", res.data)
-                console.log("typeof res.data =", typeof res.data)
                 this.recipes = res.data
                 
             } catch (err) {

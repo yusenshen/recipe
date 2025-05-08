f789ca22f9eb9a19efa0afc5db9cfa4f9f3e2771
yusenshen <yusenshen0831@gmail.com>
2025-05-08 18:57:14 +0800

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://40ff-116-15-88-34.ngrok-free.app/api',
+  baseURL: ' https://827d-116-15-88-34.ngrok-free.app/api',
   timeout: 5000,
 })
 
